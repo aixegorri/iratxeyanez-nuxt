d2bc4456c8151b5c24ff55b15eaaa2e2f89a19b3
Karlos <aixegorri@gmail.com>
2019-03-24 20:23:07 +0100

    Add postcss

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,6 +58,21 @@ export default {
           exclude: /(node_modules)/
         })
       }
+    },
+    postcss: {
+      // Add plugin names as key and arguments as value
+      // Install them before as dependencies with npm or yarn
+      plugins: {
+        // Disable a plugin by passing false as value
+        'postcss-url': false
+      },
+      preset: {
+        // Change the postcss-preset-env settings
+        autoprefixer: {
+          grid: true
+        },
+        stage: 0
+      }
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -34,7 +34,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="postcss">
 .container {
   margin: 0 auto;
   min-height: 100vh;
