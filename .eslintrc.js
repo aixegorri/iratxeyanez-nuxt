b30d0dfbee7e6f99924a5e07cf6acc9ae3976c26
Karlos <aixegorri@gmail.com>
2019-03-25 11:02:38 +0100

    Add eslint vue standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  // add your custom rules here
-  rules: {
-  }
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	extends: [
+		// '@nuxtjs',
+		// 'plugin:nuxt/recommended'
+		'@vue/standard',
+		'plugin:vue/essential'
+	],
+	// add your custom rules here
+	rules: {
+		// Indentation
+		'indent': ['error', 'tab'],
+		// Allow tabs
+		'no-tabs': ['error', { allowIndentationTabs: true }]
+	},
 }
